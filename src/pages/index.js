37a5294e99b6245a161444de2fc1034834d90ac1
Mon Patrick <monpatrickg@fligno.com.ph>
2019-09-03 12:58:46 +0800

    Enhance design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import logo from "../images/doc.png"
+// import logo from "../images/doc.png"
 
 const IndexPage = () => (
   <Layout>
     <div class="main-background-custom">
-      <img src={logo} alt="Fligno Documentation" />
+      {/* <img src={logo} alt="Fligno Documentation" /> */}
       <h1>FLIGNO DOCUMENTATION</h1>
     </div>
   </Layout>
